e906a152785a80196b0d188ca6d75bebb17b8a9e
C.Wong <channingwong@qq.com>
2023-09-23 12:01:26 +0800

    调整 reader 处理异常 case

--- gpt/reader.py
+++ gpt/reader.py
@@ -22,23 +22,28 @@ def get_paragraphs_text(ele: ChromiumElement, tag_name: str):
 
 
 def token_size(text):
+    if not text:
+        return 0
     tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
     tokens = tokenizer.tokenize(text)
     return len(tokens)
 
 
 def summarize_article(browser, title, paragraphs_text):
+    content_token = token_size(paragraphs_text)
+    if not content_token:
+        print(f"{title} has empty paragraph text")
+        return
     template_dir = os.path.expanduser('~/.my_py_datas/chatgpt/templates')
     instruction_prompt = prompts.load_prompt(os.path.join(template_dir, '文章阅读_指令.json'))
     part_content_prompt = prompts.load_prompt(os.path.join(template_dir, '文章阅读_partContent.json'))
     end_content_prompt = prompts.load_prompt(os.path.join(template_dir, '文章阅读_endContent.json'))
 
-    my_gpt.ask_as_new_chat_and_wait(browser, instruction_prompt.format(article_name=title))
     prompt_token_size = max([token_size(part_content_prompt.format(content='')),
                              token_size(end_content_prompt.format(content=''))])
     token_limit = 4096 - prompt_token_size
-    total_token = token_size(paragraphs_text)
-    text_len_limit = int(len(paragraphs_text) / total_token * token_limit)
+    my_gpt.ask_as_new_chat_and_wait(browser, instruction_prompt.format(article_name=title))
+    text_len_limit = int(len(paragraphs_text) / content_token * token_limit)
     text = ''
     for line in paragraphs_text.split('\n'):
         if len(line) > text_len_limit:
@@ -53,9 +58,12 @@ def summarize_article(browser, title, paragraphs_text):
 
 
 def read_info_q_articles(browser: ChromiumPage):
-    read_all_page_articles(browser, 'https://www.infoq.cn/article',
-                           lambda page: (page.ele('tag:h1').text,
-                                         get_paragraphs_text(page.ele('.content-main')('.article-preview'), 'p')))
+    def get_article_content(bws: ChromiumPage):
+        return (bws.ele('tag:h1').text,
+                get_paragraphs_text(bws.ele('.content-main')('.article-preview'), 'p'))
+
+    read_all_page_articles(browser, 'https://www.infoq.cn/article', get_article_content)
+    read_all_page_articles(browser, 'https://www.infoq.cn/news/', get_article_content)
 
 
 def read_wx_articles(browser: ChromiumPage):
@@ -72,14 +80,24 @@ def read_all_page_articles(browser, article_url_prefix,
                            article_content_func: Callable[[ChromiumPage], tuple[str, str]]):
     tabs = my_ch.find_all_tab_id(browser, article_url_prefix)
     if not tabs:
-        raise my_ch.TabNotFoundException('Cannot find any article tab')
+        print('Cannot find any article tab', article_url_prefix)
+        return
     for idx, tab in enumerate(tabs):
         browser.to_tab(tab)
         title, paragraphs = article_content_func(browser)
+        if not title or not paragraphs:
+            print('No title or paragraph content', browser.url)
+            continue
         summarize_article(browser, title, paragraphs)
         if idx + 1 < len(tabs):
             while tab in my_ch.find_all_tab_id(browser, article_url_prefix):
                 time.sleep(1)
+
+
+def open_info_q_mail_urls(browser: ChromiumPage):
+    urls = {x.attr('href') for x in browser.eles('tag:a@href:https://etrack01')}
+    for u in urls:
+        browser.new_tab(url=u, switch_to=False)
 
 
 if __name__ == '__main__':
